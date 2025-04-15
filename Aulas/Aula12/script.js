ad5201803e5be8d3e3b9871cdc11bbf1004ed4fe
franciscoMello777 <franciscopesdemello@gmail.com>
2025-04-15 16:57:36 -0300

    atualizando

--- Aulas/Aula12/script.js
+++ Aulas/Aula12/script.js
@@ -1,1 +1,17 @@
 alert("hello world!");
+
+let titulo = document.querySelector("#titulo");
+titulo.textContent = "Boa noite!";
+titulo.style.color = "red";
+
+function mostraAlerta() {
+    alert("funciona");
+}
+
+titulo.onclick = mostraAlerta;
+
+function mostraTexto(texto) {
+    alert(texto);
+}
+
+titulo.onclick = mostraTexto("palco");
