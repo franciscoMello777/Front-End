9453e960ba038a8e3afb9cf8b800e936f212883a
franciscoMello777 <franciscopesdemello@gmail.com>
2025-05-08 17:10:46 -0300

    atualizando

--- Aulas/Aula18/script.js
+++ Aulas/Aula18/script.js
@@ -13,40 +13,59 @@ let next = document.querySelector("#proximo")
 
 let numPokedex = 1
 
-const fetchPoke = async(pokemon) =>{
+const fetchPoke = async (pokemon) => {
     const APIresponse = await fetch(`https://pokeapi.co/api/v2/pokemon/
 ${pokemon}`);
     const data = await APIresponse.json()
     return data
 }
 
-const show = async(pokemon)=>{
+const show = async (pokemon) => {
     const dataPoke = await fetchPoke(pokemon)
-    imgPoke.src = dataPoke.sprites.front_default
+    segundaImagem(dataPoke.sprites.other.showdown.front_default, dataPoke.sprites.front_default);
     nome.innerHTML = dataPoke.name
     id.innerHTML = dataPoke.id
     tipo1.innerHTML = dataPoke.types[0].type.name
     tipo2.innerHTML = dataPoke.types[1].type.name
     habilidade.innerHTML = dataPoke.abilities[0].ability.name
-    peso.innerHTML = dataPoke.weight
-    altura.innerHTML = dataPoke.heigth
+    peso.innerHTML = dataPoke.weight / 10 + "kg"
+    altura.innerHTML = dataPoke.height / 10 + "m"
+    const audio = new Audio(dataPoke.cries.latest);
+    audio.play();
+    if (dataPoke.types[1] != undefined) {
+        tipo2.innerHTML = dataPoke.types[1].type.name;
+    } else {
+        tipo2.innerHTML = "";
+    }
 }
 
-form.addEventListener("submit", (e)=> {
+function segundaImagem(gif, image) {
+    imgPoke.src = gif;
+
+    imgPoke.onerror = function () {
+        this.onerror = null;
+        this.src = image;
+    }
+
+    return;
+}
+
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     show(inputf.value.toLowerCase());
 })
 
-back.addEventListener("click"),(e)=> {
-    if (numPokedex > 1){
-        numPokedex--
+back.addEventListener("click", (e) => {
+    if (numPokedex > 1) {
+        numPokedex--;
     }
-    show(numPokedex)
-}
+    show(numPokedex);
+});
 
-next.addEventListener("click"),(e)=> {
-    if (numPokedex < 1000){
-        numPokedex++
+next.addEventListener("click", (e) => {
+    if (numPokedex < 1000) {
+        numPokedex++;
     }
-    show(numPokedex)
-}
+    show(numPokedex);
+});
+
